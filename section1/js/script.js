36c085d8dd919e70f737ada0bad07b2c67277148
Zakar <zakardtaylor@gmail.com>
2026-02-19 13:55:47 -0500

    Added new section to the site

--- section1/js/script.js
+++ section1/js/script.js
@@ -19,3 +19,24 @@ var company = {
 };
 
 console.log(company.owner)
+
+function dialougueMaker(name){
+    var func = function(dialougue){
+        return name+": " + dialougue
+    };
+    
+    return func
+}
+
+var dialougue = dialougueMaker("Bill");
+console.log(dialougue("is my stove still on?"))
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = function(){
+    console.log(Dog(sam) + " likes barking! Bark!");
+}
+
+
